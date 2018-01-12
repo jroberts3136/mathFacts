919c74b45942998f84cce2cf30a225be91f3809d
jroberts3136 <Jroberts3136@gmail.com>
2018-01-12 08:43:15 -0500

    Finished

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,8 +16,45 @@ public class Main {
             "Press 8 for last track\n" +
             "Press anything else to end the program");
 
-        Scanner input = new Scanner(System.in);
+        Scanner keyInput = new Scanner(System.in);
         MusicMan theMusicMan = new MusicMan();
 
+        boolean notDone = true;
+        String input;
+
+        while (notDone){
+            input = keyInput.nextLine();
+            switch (input){
+                case "0":
+                    System.out.println(theMusicMan.song());
+                    break;
+                case "1":
+                    System.out.println(theMusicMan.artist());
+                    break;
+                case "2":
+                    System.out.println(theMusicMan.album());
+                    break;
+                case "3":
+                    System.out.println(theMusicMan.genre());
+                    break;
+                case "4":
+                    System.out.println(theMusicMan.length());
+                    break;
+                case "5":
+                    theMusicMan.play();
+                    break;
+                case "6":
+                    theMusicMan.pause();
+                    break;
+                case "7":
+                    theMusicMan.nextTrack();
+                    break;
+                case "8":
+                    theMusicMan.lastTrack();
+                    break;
+                default:
+                    notDone = false;
+            }
+        }
     }
 }
